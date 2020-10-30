bbdc45ad7bd371bdb3921187e4afca436b8919ac
ephantus <ephantokumu98@gmail.com>
2020-10-31 01:49:25 +0300

    second commit

---
+++ MovieDatabaseCenter/Data/MovieDatabaseCenterContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieDatabaseCenter.Models;
+
+namespace MovieDatabaseCenter.Data
+{
+    public class MovieDatabaseCenterContext : DbContext
+    {
+        public MovieDatabaseCenterContext (DbContextOptions<MovieDatabaseCenterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieDatabaseCenter.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MovieDatabaseCenter/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MovieDatabaseCenter.Data;
+using MovieDatabaseCenter.Models;
+
+namespace MovieDatabaseCenter.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MovieDatabaseCenter.Data.MovieDatabaseCenterContext _context;
+
+        public IndexModel(MovieDatabaseCenter.Data.MovieDatabaseCenterContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
