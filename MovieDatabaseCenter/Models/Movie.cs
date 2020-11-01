e4e73a59939caf85cf1b0b96d4df89ee10bce62c
ephantus <ephantokumu98@gmail.com>
2020-11-01 13:36:24 +0300

    ??

--- MovieDatabaseCenter/Models/Movie.cs
+++ MovieDatabaseCenter/Models/Movie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MovieDatabaseCenter.Models
 {
@@ -11,9 +12,12 @@ namespace MovieDatabaseCenter.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
